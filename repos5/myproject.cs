d262abafbf8db26e974295808f014bfedd6e6b5a
moeinheydaridev <moeinheydaridev@gmail.com>
2024-07-29 17:02:20 +0330

    Add files via upload

---
+++ repos5/myproject.cs
@@ -0,0 +1,55 @@
+﻿namespace ConsoleApp8
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        { 
+            {
+                car car1 = new car();
+                car1.carname = "peykan";
+                car1.carspeed = "20000";
+                car1.carmodel = "96";
+
+                car car2 = new car();
+                car2.carname = "pride";
+                car2.carspeed = "100";
+                car2.carmodel = "1400 \n ";
+
+                Console.WriteLine(car1.carname + " " + car1.carspeed + " " + car1.carmodel);
+                Console.WriteLine(car2.carname + " " + car2.carspeed + " " + car2.carmodel)
+
+
+                person person1 = new person("yara", "nourkhani", "20");
+                person person2 = new person("moein", "heydari", "20");
+
+
+                Console.WriteLine($" name : {person1.name} \n familyname : {person1.familyname}  \n age : {person1.age} \n ");
+
+                Console.WriteLine($" name : {person2.name}  \n familyname : {person2.familyname} \n  age : {person2.age}");
+
+                Console.ForegroundColor = ConsoleColor.Green;
+
+                Console.WriteLine("**********************");
+
+                Console.ResetColor();
+
+                Console.WriteLine("please enter your number:");
+
+                int number = Convert.ToInt32(Console.ReadLine());
+                person[] people = new person[number];
+
+                for (int i = 0; i < number; i++) ;
+
+                Console.ForegroundColor = ConsoleColor.Green;
+
+                Console.WriteLine("**********************");
+
+                Console.ResetColor();
+
+
+            }
+        }
+    }
+}
+
---
+++ repos5/person.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp8
+{
+    internal class person
+    {
+        internal object name;
+        internal object familyname;
+        internal object age;
+
+        public person(string v1, string v2, string v3)
+        {
+            name = v1;
+            familyname = v2;
+            age = v3;
+        }
+    }
+}
